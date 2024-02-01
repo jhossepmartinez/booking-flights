337301d1713cfb53315be7c5d651e1e4bd9e7ac0
jhossepmartinez <jhossepmv@gmail.com>
2024-02-01 13:17:07 -0300

    added flights connection to database and view in frontend

---
+++ src/components/Flights/displayLogo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function renderAirlineLogo(airlineName) {
+    if (airlineName === "Air France") {
+        return (
+            <img
+                className="absolute top-0"
+                src="../../../public/air-france.svg"
+            />
+        );
+    } else if (airlineName === "United Airlines") {
+        return (
+            <img
+                className=" absolute top-0 flex-shrink-0"
+                src="../../../public/united-airlines.svg"
+            />
+        );
+    }
+}
